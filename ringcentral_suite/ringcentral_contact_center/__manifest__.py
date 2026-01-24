7cefe5919ac7eee987775aa602f524781aa75f1f
Abel Eyasu <80050691+abeleyasu@users.noreply.github.com>
2026-01-24 20:39:00 +0300

    Bump RingCentral module versions

--- ringcentral_suite/ringcentral_contact_center/__manifest__.py
+++ ringcentral_suite/ringcentral_contact_center/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'RingCentral Contact Center',
-    'version': '18.0.1.0.0',
+    'version': '18.0.1.0.1',
     'category': 'Phone',
     'summary': 'Contact center features - ACD, IVR, queues',
     'description': """
